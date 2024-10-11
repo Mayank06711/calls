c709144eaca3ebc7ac7b5f53c18318586a97ef98
Mayank Soni <mayank93soni@gmail.com>
2024-10-12 04:47:02 +0530

    define feedback routes:

---
+++ server/src/routes/feedbackRoutes.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import FeedbackController from "../controllers/feedbackController";
+import { Middleware } from "../middlewares/middlewares";
+const router = express.Router();
+
+// Create Feedback
+router.post(
+  "/new",
+  Middleware.VerifyJWT,
+  FeedbackController.createFeedback
+);
+
+// Get Feedbacks (filtered by criteria)
+router.get("/find", Middleware.VerifyJWT, FeedbackController.getFeedback);
+
+// Update Feedback (by feedbackId)
+router.put(
+  "/:feedbackId",
+  Middleware.VerifyJWT,
+  FeedbackController.updateFeedback
+);
+
+export default router;
