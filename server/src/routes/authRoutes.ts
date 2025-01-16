efbcc22ed499ea85ea199ca67d95d6cecd8ebe9e
Mayank Soni <mayank93soni@gmail.com>
2025-01-16 23:59:20 +0530

    add refreshToken routes

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import Authentication from "../controllers/authController";
-
+import { AuthServices } from "../helper/auth";
 const router = express.Router();
 
 // Create Feedback
@@ -8,5 +8,8 @@ router.post("/generate_otp", Authentication.generateOtp);
 
 // Get Feedbacks (filtered by criteria)
 router.post("/verify_otp", Authentication.verifyOtp);
+
+// refreshAccessToken
+router.post("/refresh_token", AuthServices.RefreshAccessToken)
 
 export default router;
