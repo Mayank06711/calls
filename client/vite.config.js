07e0e6ba16b1c7987c47ebfac378ff330222e7bb
Mayank Soni <mayank93soni@gmail.com>
2025-01-22 23:28:23 +0530

    fixed env problem

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,14 @@ import react from "@vitejs/plugin-react";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import env from "./src/config/env.config";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    https: process.env.USE_HTTPS
+    https: env.USE_HTTPS
       ? {
           key: fs.readFileSync(path.resolve(__dirname, "./cert/cert.key")),
           cert: fs.readFileSync(path.resolve(__dirname, "./cert/cert.crt")),
@@ -18,7 +19,7 @@ export default defineConfig({
     host: "localhost",
     port: 3000,
   },
-  define: { "process.env": process.env },
+  define: { "process.env": env },
   // envDir: ".env",
 });
 // # For HTTPS: run this command
