67626150b05fc925002a949b8a6a9726baf659b3
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2025-02-01 22:22:08 +0530

    create file structure for the dashboard

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,5 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Box, Container,Gritd2, useTheme } from "@mui/material";
+import ChatSection from "../Chat/ChatSection";
+import VideoCall from "../VideoCall/VideoCall";
+import SubscriptionPanel from "../Subscription/SubscriptionPanel";
+import NotificationPanel from "../Notifications/NotificationPanel";
 import UserProfile from "../User/UserProfile";
+import AIAssistant from "../AI/AIAssistant";
+import ReelsSection from "../Reels/ReelsSection";
+
 function Home() {
   return <UserProfile />;
 }
