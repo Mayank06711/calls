5f0571a16a8e726587d86fd49e40cb598d838a4c
Mayank Soni <mayank93soni@gmail.com>
2025-02-05 03:42:00 +0530

    added email routes

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -5,10 +5,11 @@ const router = Router();
 
 // User routes
 router.route("/signup").post(User.signUp);
-router.route("/verify-email").post(User.verifyEmail);
+router.route("/email_verify/:token").get(User.verifyEmailToken);
 router.route("/login").post(User.login);
 // Secured routes
 router.use(Middleware.VerifyJWT); // Apply JWT middleware to all routes below
+router.route("/email_verification").post(User.verifyEmail);
 router.route("/profile").get(User.getProfile).patch(User.updateProfile); // Combined profile routes
 router.route("/password").post(User.forgotPassword).patch(User.changePassword); // Combined password routes
 router.route("/logout").post(User.logout);
