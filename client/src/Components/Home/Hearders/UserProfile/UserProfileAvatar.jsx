1b0572f817ac6df4d3c073cbb09bf94a339c790b
vishalkushwaha2003 <vishal2003kushwaha@gmail.com>
2025-02-04 06:38:19 +0530

    fix bug

--- client/src/Components/Home/Hearders/UserProfile/UserProfileAvatar.jsx
+++ client/src/Components/Home/Hearders/UserProfile/UserProfileAvatar.jsx
@@ -2,7 +2,7 @@ import { Avatar, IconButton } from '@mui/material'
 import React from 'react'
 import { useSubscriptionColors } from '../../../../utils/getSubscriptionColors'
 import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchUserInfoThunk } from '../../../../redux/thunks/userInfo.thunks';
 import { setProfileDataLoading } from '../../../../redux/actions';
 
@@ -10,9 +10,15 @@ function UserProfile() {
   const colors=useSubscriptionColors();
   const navigate=useNavigate();
   const dispatch= useDispatch();
+  const userinfo =useSelector((state) => state.userInfo.data);
   const handleClick=(path)=>{
+    if(userinfo){
+      navigate(path);
+      return;
+    };
     dispatch(setProfileDataLoading(true));
     navigate(path);
+    
     dispatch(fetchUserInfoThunk());
   }
   return (
