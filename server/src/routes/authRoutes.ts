1deceefcb532522f255a169ae02b3b5ef290465b
Mayank Soni <mayank93soni@gmail.com>
2025-02-28 20:17:53 +0530

    route for ai

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import Authentication from "../controllers/authController";
 import { AuthServices } from "../helper/auth";
+import { Middleware } from "../middlewares/middlewares";
 const router = express.Router();
 
 // Create Feedback
@@ -10,7 +11,12 @@ router.post("/generate_otp", Authentication.generateOtp);
 router.post("/verify_otp", Authentication.verifyOtp);
 
 // refreshAccessToken
-router.post("/refresh_token", AuthServices.RefreshAccessToken)
+router.post("/refresh_token", AuthServices.RefreshAccessToken);
 
-router.post("/process", AuthServices.verifyAndForwardToAI)
+// Protected route - needs authentication
+router.post(
+  "/process",
+  Middleware.VerifyJWT,
+  AuthServices.verifyAndForwardToAI
+);
 export default router;
