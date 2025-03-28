1fd6f41668b35d4f787db449c6630eb2cb076702
vishalkushwaha2003 <vishal@mapsense.in>
2025-03-29 04:36:18 +0530

    create action for subscription

--- client/src/redux/actions/subscription.action.js
+++ client/src/redux/actions/subscription.action.js
@@ -1,6 +1,11 @@
-import { SET_SUBSCRIPTION_TYPE } from '../action_creators/subscription.action_creators';
+import { SET_SUBSCRIPTION_TYPE,GET_SUBSCRIPTION_PLANS } from '../action_creators/subscription.action_creators';
 
 export const setSubscriptionType = (subscriptionType) => ({
   type: SET_SUBSCRIPTION_TYPE,
   payload: subscriptionType,
+});
+
+export const getSubscriptionPlans = (payload) => ({
+  type: GET_SUBSCRIPTION_PLANS,
+  payload: payload,
 });
