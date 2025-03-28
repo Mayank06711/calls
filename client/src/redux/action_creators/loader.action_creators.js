e3a706d18e9d507708eec1f4f52a9accc789f2a6
vishalkushwaha2003 <vishal@mapsense.in>
2025-03-29 04:38:48 +0530

    add loader creators

--- client/src/redux/action_creators/loader.action_creators.js
+++ client/src/redux/action_creators/loader.action_creators.js
@@ -7,5 +7,13 @@ export const LOADER_TYPES = {
     FETCH_USER: 'fetchUser',
     UPDATE_PROFILE: 'updateProfile',
     AI_CHAT_PROCESS: 'aiChatProcess',
+    SUBSCRIPTION_CREATE: 'subscriptionCreate',
+    SUBSCRIPTION_CURRENT: 'subscriptionCurrent',
+    SUBSCRIPTION_CANCEL: 'subscriptionCancel',
+    SUBSCRIPTION_UPDATE_PAYMENT: 'subscriptionUpdatePayment',
+    SUBSCRIPTION_GET_PLANS: 'subscriptionGetPlans',
+    SUBSCRIPTION_HISTORY: 'subscriptionHistory',
+    SUBSCRIPTION_DETAILS: 'subscriptionDetails',
+    SUBSCRIPTION_CONFIG: 'subscriptionConfig',
     // ... add more loader types as needed
   };
